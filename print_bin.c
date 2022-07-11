375e10b7d84b2cc266974ea6b00a2ecaafa9ccb8
pius2001 <gabrielpius0311@gmail.com>
2022-07-11 06:51:53 -0700

    added

---
+++ print_bin.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include "main.h"
+
+
+void convert_and_print(unsigned int num, int *count);
+
+/**
+ * print_bin - prints int to binary
+ * b: pointer to unsigned int
+ * Return: integer, number of characters printed
+ */
+
+int print_bin(va_list b)
+{
+	unsigned int num = va_arg(b, unsigned int);
+	int count = 0;
+
+	convert_and_print(num, &count);
+
+	return (count);
+}
+
+/**
+ * convert_and_print - converts int to binary, then prints
+ * @num: unsigned int
+ * @count: pointer to count the iteration
+ */
+
+void convert_and_print(unsigned int num, int *count)
+{
+	if (num / 2)
+		convert_and_print(num / 2, count);
+
+	_putchar((num % 2) + '0');
+
+	(*count)++;
+}
---
+++ print_char.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_char - prints character
+ * @c: char to be prited
+ * Return 1
+ */
+
+int print_char(va_list c)
+{
+unsigned char my_ch;
+my_ch = va_arg(c, int);
+_putchar(my_ch);
+return (1);
+}
+
+/**
+ * print_percent - prints persentage
+ * Return: 1
+ */
+int print_percent(void)
+{
+_putchar('%');
+return (1);
+}
